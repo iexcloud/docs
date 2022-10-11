51af2ebfadf8a8b93448ff6e529f214b86b7248a
jhinkey-iex <jim.hinkey@iexcloud.io>
2022-10-11 14:39:39 +0000

    Revert "PRD-119 config analytics. Add script to home pg."
    This reverts commit a300ebf089f269731c0aa79127cbcaa169aeb459.

--- source/conf.py
+++ source/conf.py
@@ -40,7 +40,6 @@ extensions = [
     "sphinx_design",
     "sphinx_inline_tabs",
     "sphinxext.rediraffe",
-    "sphinxcontrib.gtagjs",
 ]
 
 # -- Options for Markdown files ----------------------------------------------
@@ -289,9 +288,3 @@ texinfo_documents = [
 # -- Options for Rediraffe redirects with Rederaffe ----------------------------
 
 rediraffe_redirects = "redirects.txt"
-
-# -- Googleanalytics --------------------
-
-gtagjs_ids = [
-  'G-M0MJ9E8ZP5',
-]
